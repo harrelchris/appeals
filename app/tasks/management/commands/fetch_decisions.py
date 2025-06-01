ecd7799b62362a151c4b2ceb26c62ed84531bcaa
Chris Harrel <harrelchris@gmail.com>
2025-06-01 04:09:22 -0500

    Create fetch_decisions.py

---
+++ app/tasks/management/commands/fetch_decisions.py
@@ -0,0 +1,52 @@
+from concurrent.futures import as_completed
+
+from requests_futures.sessions import FuturesSession
+
+from django.core.management.base import BaseCommand
+
+from decisions.models import Decision, URL
+from tasks.models import Task
+
+TASK_NAME = "fetch-decisions"
+
+session = FuturesSession()
+
+
+class Command(BaseCommand):
+    help = "Fetch new Decisions"
+
+    def handle(self, *args, **options):
+        msg = "Fetching new Decisions"
+        self.stdout.write(self.style.SUCCESS(msg))
+
+        urls = URL.objects.exclude(id__in=Decision.objects.values("url_id"))
+        url_list = list(urls)
+        chunk_size = 100
+        url_chunks = [url_list[i:i + chunk_size] for i in range(0, len(url_list), chunk_size)]
+
+        for chunk in url_chunks:
+            start = chunk[0].id
+            stop = chunk[-1].id
+            msg = f"Requesting decisions: {start} - {stop}"
+            self.stdout.write(self.style.SUCCESS(msg))
+
+            futures = []
+            for url in chunk:
+                future = session.get(url.loc)
+                future.url = url
+                futures.append(future)
+
+            decisions = []
+            for future in as_completed(futures):
+                response = future.result()
+                decision = Decision(
+                    url=future.url,
+                    text=response.text,
+                )
+                decisions.append(decision)
+
+            decisions = sorted(decisions, key=lambda d: d.url_id)
+            Decision.objects.bulk_create(decisions)
+
+        task = Task(name=TASK_NAME, success=True)
+        task.save()
