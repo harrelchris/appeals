9e9e6daa7528355506868a03acb48fc51e97331c
Chris Harrel <harrelchris@gmail.com>
2025-06-07 15:31:47 -0500

    Implement search using Postgres

--- app/decisions/urls.py
+++ app/decisions/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = "decisions"
 
 urlpatterns = [
+    path("", view=views.DecisionListView.as_view(), name="list"),
+    path("search/", view=views.DecisionSearchView.as_view(), name="search"),
+    path("results/", view=views.DecisionResultsView.as_view(), name="results"),
     path("<int:pk>/", views.DecisionDetailView.as_view(), name="detail"),
 ]
--- app/decisions/views.py
+++ app/decisions/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView, TemplateView
+from django.contrib.postgres.search import SearchHeadline, SearchQuery, SearchRank, SearchVector
 
 from decisions.models import Decision
 
@@ -7,3 +8,33 @@ class DecisionDetailView(DetailView):
     template_name = "decisions/detail.html"
     model = Decision
     context_object_name = "decision"
+
+
+class DecisionListView(ListView):
+    template_name = "decisions/list.html"
+    model = Decision
+    context_object_name = "decisions"
+    paginate_by = 100
+
+
+class DecisionSearchView(TemplateView):
+    template_name = "decisions/search.html"
+
+
+class DecisionResultsView(ListView):
+    template_name = "decisions/list.html"
+    model = Decision
+    context_object_name = "decisions"
+    paginate_by = 100
+
+    def get_queryset(self):
+        q = self.request.GET.get("q", "")
+        vector = SearchVector("text")  # can include multiple columns: SearchVector("text", "title")
+        query = SearchQuery(q)
+        headline = SearchHeadline("text", query)
+
+        # return Decision.objects.filter(text__search=q)
+        # return Decision.objects.annotate(search=vector).filter(search=query)
+        # return Decision.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+        # return Decision.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gte=0.001).order_by("-rank")
+        return Decision.objects.annotate(rank=SearchRank(vector, query)).annotate(headline=headline).filter(rank__gte=0.001).order_by("-rank")
